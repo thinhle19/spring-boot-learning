f98c7ef2332d13f2bb191314b6e162d6f2d43060
Thinh Le <letienthinh0109@gmail.com>
2023-09-24 17:49:57 +0700

    primary annotation

--- springcore/src/main/java/com/thinhle/springcore/rest/DemoController.java
+++ springcore/src/main/java/com/thinhle/springcore/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.thinhle.springcore.rest;
 
 import com.thinhle.springcore.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +11,14 @@ public class DemoController {
     // define private field for the dependency
     private Coach myCoach;
 
-   /* // constructor for dpdc injection
+    // constructor for dpdc injection
     @Autowired // tell Spring to inject dependency
     // optional if there's just 1 constructor
-    public DemoController(Coach theCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
         myCoach = theCoach;
     }
-*/
 
-    @Autowired // can be any method name
+/*    @Autowired // can be any method name
     public void setCoach(Coach theCoach) {
         myCoach = theCoach;
     }
@@ -27,7 +27,7 @@ public class DemoController {
     @Autowired
     public void aaa(Coach theCoach) {
         myCoach = theCoach;
-    }
+    }*/
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
