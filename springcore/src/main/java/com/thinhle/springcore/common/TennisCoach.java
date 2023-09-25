ae229041cc578987503756be5a71329bcf74a727
Thinh Le <letienthinh0109@gmail.com>
2023-09-25 20:43:22 +0700

    primary & qualifier

---
+++ springcore/src/main/java/com/thinhle/springcore/common/TennisCoach.java
@@ -0,0 +1,16 @@
+package com.thinhle.springcore.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+// Spring is dynamic => IDE may not be able to determine of a given class/method is used at runtime
+// => why there's "no usages" warning.
+@Component // mark the class as a Spring Bean => make it available for dependency injection.
+@Primary
+public class TennisCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Wake up and do Tennis Coach";
+    }
+}
