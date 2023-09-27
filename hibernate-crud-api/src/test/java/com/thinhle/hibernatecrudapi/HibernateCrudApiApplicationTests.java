1228c00d3eae122351ce5d3205ca0c9752b97034
Thinh Le <letienthinh0109@gmail.com>
2023-09-27 11:36:26 +0700

    init

---
+++ hibernate-crud-api/src/test/java/com/thinhle/hibernatecrudapi/HibernateCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinhle.hibernatecrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
