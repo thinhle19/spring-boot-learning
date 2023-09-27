9bba19ee9744c62247de017e408e6e1c76ee04a3
Thinh Le <letienthinh0109@gmail.com>
2023-09-27 13:04:47 +0700

    init

---
+++ demo-crm/src/main/java/com/thinhle/democrm/DemoCrmApplication.java
@@ -0,0 +1,13 @@
+package com.thinhle.democrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCrmApplication.class, args);
+	}
+
+}
---
+++ demo-crm/src/test/java/com/thinhle/democrm/DemoCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinhle.democrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
