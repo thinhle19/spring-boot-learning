9836605857bd7d2aef13269fa068a98f6ce3c70d
Thinh Le <letienthinh0109@gmail.com>
2023-09-27 12:36:11 +0700

    exception handling

--- hibernate-crud-api/src/main/java/com/thinhle/hibernatecrudapi/rest/StudentRestController.java
+++ hibernate-crud-api/src/main/java/com/thinhle/hibernatecrudapi/rest/StudentRestController.java
@@ -2,6 +2,8 @@ package com.thinhle.hibernatecrudapi.rest;
 
 import com.thinhle.hibernatecrudapi.entity.Student;
 import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -27,6 +29,34 @@ public class StudentRestController {
 
     @GetMapping("/students/{id}")
     public Student getStudentById(@PathVariable int id) {
+        if (id >= students.size() || id < 0) {
+            throw new StudentNotFoundException(String.format("Student %d can't be found", id));
+        }
+
         return students.get(id);
     }
+
+    // Exception Handler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException e) {
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(e.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception e) {
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(e.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 }
+
